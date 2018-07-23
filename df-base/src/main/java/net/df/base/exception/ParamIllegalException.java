9a0936746c91181bd769d88db0a85167937ceb35
wxc <wxc252@github.com>
2018-07-23 23:02:37 +0800

    删除并重新梳理

--- df-base/src/main/java/net/df/base/exception/ParamIllegalException.java
+++
@@ -1,9 +0,0 @@
-package net.df.base.exception;
-
-import net.df.base.constants.ErrorNo;
-
-public class ParamIllegalException extends  BusinessException{
-    public ParamIllegalException(String message){
-        super(ErrorNo.VALIDATE_FAIL, message);
-    }
-}
